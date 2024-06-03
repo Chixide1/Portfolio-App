07a62adf7a2ba571a9b18b34aec1b2f9c75be993
Chik <chixide12@gmail.com>
2024-06-04 00:10:51 +0100

    created Sidenavs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarsComponent } from './navbars/navbars.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
