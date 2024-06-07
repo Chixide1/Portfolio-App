3e304784e90141c06a02cb00e02aeabcc0438e96
Chik <chixide12@gmail.com>
2024-06-07 01:22:32 +0100

    Add home content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarsComponent } from './navbars/navbars.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarsComponent],
+  imports: [RouterOutlet, NavbarsComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
