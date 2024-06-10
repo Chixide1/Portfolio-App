ba4c26a3543574bea15fb81044edb8a0140b36c0
Chik <chixide12@gmail.com>
2024-06-10 23:29:31 +0100

    Finished the About me section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatChipsModule} from '@angular/material/chips';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MatChipsModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.sass'
 })
