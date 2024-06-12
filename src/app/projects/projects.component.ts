225b8e53ecfab62e7ec119a7931ede9e48ae16b6
Chik <chixide12@gmail.com>
2024-06-12 22:53:40 +0100

    creating a slider with sample data

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { Product, products } from "./product";
+import { NgFor } from '@angular/common';
+import { empty } from 'rxjs';
 
 @Component({
   selector: '[app-projects]',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.sass'
 })
 export class ProjectsComponent {
+  products: Product[] = products
+  currentItem: Product
 
+  constructor(){
+    this.currentItem = products[0]
+  }
+
+  getCurrentItem = (index: number) => {
+    this.currentItem = products[index]
+  }
 }
