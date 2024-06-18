d59e7c0ba7dcb3802751836bb8751219025ff964
Chik <chixide12@gmail.com>
2024-06-18 21:00:12 +0100

    added mat tabs

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,29 +2,17 @@ import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
 import data from './projects.json';
 import { RouterLink } from '@angular/router';
+import {MatTabsConfig, MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: '[app-projects]',
   standalone: true,
-  imports: [NgFor, RouterLink],
+  imports: [NgFor, RouterLink, MatTabsModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.sass'
 })
 export class ProjectsComponent {
   projects: Project[] = data
-  currentItem: Project
-  selected: any
-
-  constructor(){
-    this.currentItem = this.projects[0]
-    this.selected = this.currentItem.name
-  }
-
-  getCurrentItem = (name: string) => {
-    const index = this.projects.findIndex(i => i.name == name)
-    this.currentItem = this.projects[index]
-    this.selected = this.currentItem.name
-  }
 }
 
 type Project = {
