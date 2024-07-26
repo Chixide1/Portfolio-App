043e3bae7b900c2a1cd6318bdddb2ea613554147
Chik <chixide12@gmail.com>
2024-07-26 23:19:48 +0100

    Added tooltips to skills

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component,HostListener } from '@angular/core';
 import {MatChipsModule} from '@angular/material/chips';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: '[app-about]',
   standalone: true,
-  imports: [MatChipsModule],
+  imports: [MatChipsModule, MatTooltipModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.sass'
 })
